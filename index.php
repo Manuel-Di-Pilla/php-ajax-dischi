9084d347ee8edd6121ece4e51c4f119e6700122c
Manuel <manueldipilla3@gmail.com>
2020-02-25 13:19:54 +0100

    struttura

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>spotify</title>
+    <link rel="stylesheet" href="dist/app.css">
+  </head>
+  <body>
+    
+  </body>
+</html>
