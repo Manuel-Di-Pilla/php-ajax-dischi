94707ccfd583e0cf4b870aa4ccbdf1a11698a041
Manuel <manueldipilla3@gmail.com>
2020-02-25 20:27:58 +0100

    restayling

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-  include __DIR__.'/database.php';
+  include __DIR__.'/server/database.php';
 ?>
 
 
---
+++ server/database.php
@@ -0,0 +1,58 @@
+<?php
+$database = [
+  [
+    'title' => 'New Jersey',
+    'author' => 'Bon Jovi',
+    'year' => 1988,
+    'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+  ],
+  [
+    'title' => 'Dance Monkey',
+    'author' => ' Tones and I',
+    'year' => 2019,
+    'poster' => 'https://m.media-amazon.com/images/I/81gCMCFSSeL._SS500_.jpg'
+  ],
+  [
+    'title' => 'Ritmo',
+    'author' => 'The Black Eyed Peas ',
+    'year' => 2020,
+    'poster' => 'https://www.newsic.it/wp-content/uploads/2019/10/bep-j.balvin-ritmo-786x786.jpg'
+  ],
+  [
+    'title' => 'Bravi a Cadere',
+    'author' => 'Marracash',
+    'year' => 2020,
+    'poster' => 'https://www.angolotesti.it/cover/289363.jpg'
+  ],
+  [
+    'title' => 'Next to me',
+    'author' => 'Imagine Dragons',
+    'year' => 2018,
+    'poster' => 'https://www.babboleo.it/wp-content/uploads/2018/04/1272137.jpg'
+  ],
+  [
+    'title' => 'Ringo Starr',
+    'author' => 'Pinguini Tattici Nucleari',
+    'year' => 2020,
+    'poster' => 'https://static.lafeltrinelli.it/static/images-3/xxl/194/13757194.jpg'
+  ],
+  [
+    'title' => 'Venom',
+    'author' => 'Eminem',
+    'year' => 2018,
+    'poster' => 'https://m.media-amazon.com/images/I/81Cmlf3ErRL._SS500_.jpg'
+  ],
+  [
+    'title' => 'Lose Your Self',
+    'author' => 'Eminem',
+    'year' => 2002,
+    'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51OypmO2ebL._SX466_.jpg'
+  ],
+  [
+    'title' => 'Ricominciamo',
+    'author' => 'Adriano Pappalardo',
+    'year' => 1998,
+    'poster' => 'https://img.discogs.com/LY9O50eC3se2j3K7xvseWIHfc3A=/fit-in/567x573/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2764131-1300353767.jpeg.jpg'
+  ]
+]
+?>
